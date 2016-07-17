41ec4878a1cf3dbb41133909929ffb8ffa9271d9
jfrmilner <jfrmilner@googlemail.com>
2016-07-17 15:36:38 +0100

    Add Blog Post URL

--- ClassicJoyStick_USB/ClassicJoyStick_USB.ino
+++ ClassicJoyStick_USB/ClassicJoyStick_USB.ino
@@ -1,5 +1,6 @@
 /* Commodore 64/Amiga, Atari 2600/XL/ST, ZX Spectrum, Amstrad CPC, MSX and many others compatible Joystick to USB
  * Auth: John Milner / jfrmilner
+ * Blog Post URL: https://jfrmilner.wordpress.com/2016/07/17/arduino-project-commodore-64amiga-atari-2600xlst-zx-spectrum-joystick-to-windowslinux-retropie-usb/
  * 
  * Version
  * 1.0
